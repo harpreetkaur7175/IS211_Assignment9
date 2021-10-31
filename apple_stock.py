6c35924555748e61b879c7ea42e82679e4212578
harpreetkaur7175 <89755899+harpreetkaur7175@users.noreply.github.com>
2021-10-31 17:59:55 -0400

    Update apple_stock.py

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,19 @@
-if __name__ == "__main__":
-    pass
+import re
+import requests as req
+from bs4 import BeautifulSoup as bs
+
+webpage = req.get('https://finance.yahoo.com/quote/AAPL/history?p=AAPL')
+content = webpage.content
+soup = bs(content,'html.parser')
+
+row_data = soup.find_all('tr', class_='BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)')
+
+for i in range(len(row_data)):
+   parse_data = row_data[i].find_all('td')
+   stock_date = parse_data[0].text
+   try:
+       stock_price_closing = 'closing price: ' + parse_data[4].text
+   except IndexError:
+       stock_price_closing = parse_data[1].text
+   print('%s, %s' % (stock_date, stock_price_closing))
+
