a11a61112dd2f226bd829d60ddb222bce6606e5e
harpreetkaur7175 <89755899+harpreetkaur7175@users.noreply.github.com>
2021-10-31 18:01:26 -0400

    Update football_stats.py

--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,36 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.cbssports.com/nfl/stats/player/scoring/nfl/regular/qualifiers/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'lxml')
+
+div = soup.find_all('div', class_='TableBase-overflow')
+
+trs = div[0].find_all('tr', class_="TableBase-bodyTr")
+# print(trs)
+
+counter = 0
+for tr in trs:
+   counter +=1
+   if counter <=20:
+       td = tr.find_all('td')
+       #get the player name
+       info = td[0].find_all('span', class_='CellPlayerName--long')
+       player_info = info[0].text
+       player_split = " ".join(player_info.split())
+       player_space = player_split.split(' ')
+
+       team = player_space[-1]
+       position = player_space[-2]
+       name = player_space[0]+ ' ' + player_space[1]
+
+       print("Player Name: " + name)
+       print("Player Position: " + position)
+       print("Player Team: " + team)
+
+       #get player points scored
+       points_td = td[12].text.strip()
+       print("Points scored: " + points_td)
+
